bb9b3eada01ead0cae163820ffe4c18a29eff395
Documenter.jl <documenter@juliadocs.github.io>
2024-05-17 06:48:15 +0000

    build based on 64b3a44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RandomMeas","category":"page"},{"location":"#RandomMeas","page":"Home","title":"RandomMeas","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RandomMeas.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RandomMeas]","category":"page"}]
+}
