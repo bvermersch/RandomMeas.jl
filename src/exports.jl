7e80f6c5569641409794856afcbe751146d12a6c
Andreas Elben <36476654+AElton363@users.noreply.github.com>
2025-09-11 14:49:30 +0200

    examples saved

--- src/exports.jl
+++ src/exports.jl
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Benoît Vermersch and Andreas Elben
+# SPDX-License-Identifier: Apache-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
 """
 RandomMeas.jl - Public API Exports
 
